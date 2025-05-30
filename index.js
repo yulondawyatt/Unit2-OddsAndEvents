ff8c509a43d9d13d6a247033998013c41eba34cd
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-05-30 08:17:49 -0400

    Got the title onto the page. Still working on putting together the form.

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+// Create an h1 node for the fomr title
+    // Add the form title to the h1 node
+// Grab the APP div and add it to the DoM
+// Create a form node for the APP div
+
+
+
+
+const oddsEventApp = document.querySelector(`#app`);
+console.log(oddsEventApp);
+
+const h1FormTitle = document.createElement('h1');
+
+h1FormTitle.innerText = 'Odds and Events';
+oddsEventApp.appendChild(h1FormTitle);
+
+// Odds and Events Form
+const formOddsEvent = document.createElement('form');
+
+
+
+
+const labelForBank = document.createElement('label');
+
+
+
+// Add elements to Bank section
+const sectionForBank = document.createElement('section');
+console.log(sectionForBank);
+const inputForBank = document.createElement('input');
+console.log(inputForBank);
+
+sectionForBank.appendChild(labelForBank);
+
+
+//Put form into app section
+oddsEventApp.appendChild(formOddsEvent);
+
+// 
+// Create section node for the Bank
+  // Create a label for the bank
+  // Create text for the label
+  // Create input for the label
